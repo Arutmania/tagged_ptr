34d1b99e79f31a0d33baaf3e96f55a8ea3bcfb37
Piotrek Kochański <khnsky@khnsky.cc>
2021-07-12 01:08:03 +0200

    add tag type

---
+++ include/tagged_ptr.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <cstdint>
+#include <exception>
+#include <type_traits>
+
+namespace tagged {
+
+template<unsigned long long Width>
+class tag {
+public:
+    static constexpr auto max  = 1 << Width;
+    static constexpr auto mask = Width - 1;
+
+    intptr_t value;
+
+    constexpr tag()
+        : tag(0, std::true_type {})
+    {}
+
+    constexpr tag(intptr_t value)
+        : tag(value, std::true_type {})
+    {}
+
+    static constexpr auto unchecked(intptr_t value) {
+        return tag { value, std::false_type {} };
+    }
+
+    struct exception : std::exception {
+        char const* what() const noexcept override {
+            return "[tagged::tag] value out of width range";
+        }
+    };
+
+private:
+    constexpr tag(intptr_t value, std::true_type)
+        : value(value)
+    {
+        if (value >= max)
+            throw exception {};
+    }
+
+    constexpr tag(intptr_t value, std::false_type)
+        : value(value)
+    {}
+};
+} // namespace tagged
